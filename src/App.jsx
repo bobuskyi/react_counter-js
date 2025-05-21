3370f07cf11aa2d9f6f820581030e7f8d63adfa6
Yurii Bobuskyi <y.bobuskyi@gmail.com>
2025-05-21 14:48:54 +0300

    React counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(prevCount => {
+      const updatedCount = prevCount + 1;
+
+      return updatedCount;
+    });
   };
 
   const add100 = () => {
-    // write code here
+    setCount(prevCount => {
+      const updatedCount = prevCount + 100;
+
+      return updatedCount;
+    });
   };
 
   // DON'T change the code below
